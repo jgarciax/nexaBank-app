ba0e292fd6f2b9e4cc523fb983f0ccae0033be7f
jared <jared@MacBook-Pro-de-jared.local>
2026-04-26 17:42:55 -0600

    feat: add KeepAliveService and implement client name search functionality in account list

--- src/app/features/cuentas/pages/cuentas-list/cuentas-list.component.ts
+++ src/app/features/cuentas/pages/cuentas-list/cuentas-list.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CuentasService } from '../../services/cuentas.service';
-import { forkJoin } from 'rxjs';
-
 
 @Component({
   selector: 'app-cuentas-list',
@@ -12,10 +10,12 @@ import { forkJoin } from 'rxjs';
 export class CuentasListComponent implements OnInit {
 
   saldos: any[] = [];
+  saldosOriginal: any[] = [];
   cuentas: any[] = [];
   loading = true;
   totalSaldo = 0;
   numeroCuenta = '';
+  nombreCliente = '';
   errorMsg = '';
 
   constructor(
@@ -31,17 +31,16 @@ export class CuentasListComponent implements OnInit {
   get cuentasActivas(): number {
     return this.cuentas.filter(c => c.estatus === 'ACTIVA').length;
   }
-    cargarSaldos(): void {
-    this.loading = true;
 
+  cargarSaldos(): void {
+    this.loading = true;
     this.cuentasService.getSaldoCuentas().subscribe({
       next: (resp: any[]) => {
-  this.saldos = resp;
+        this.saldosOriginal = resp;
+        this.saldos = resp;
         this.calcularTotal();
-
         this.loading = false;
-
-        this.cdr.detectChanges(); 
+        this.cdr.detectChanges();
       },
       error: (err) => {
         console.error(err);
@@ -55,68 +54,71 @@ export class CuentasListComponent implements OnInit {
     this.cuentasService.getCuentas().subscribe({
       next: (resp: any[]) => {
         this.cuentas = resp;
-        this.cdr.detectChanges(); 
+        this.cdr.detectChanges();
       },
-      error: (err) => {
-        console.error(err);
-      }
+      error: (err) => console.error(err)
     });
   }
 
-
-    buscarCuenta(): void {
-    if (!this.numeroCuenta) {
-      this.cargarSaldos();
-      return;
-    }
-
+buscarCuenta(): void {
+  this.errorMsg = '';
+  if (this.numeroCuenta) {
     this.loading = true;
-    this.errorMsg = '';
     this.saldos = [];
-
     this.cuentasService.getCuentaByNumero(this.numeroCuenta).subscribe({
       next: (resp: any) => {
         const dataApi = resp.data ? resp.data : resp;
-
         if (dataApi && Object.keys(dataApi).length > 0) {
-
-          const cuentaMapeada = {
+          this.saldos = [{
             numeroCuenta: dataApi.numero_cuenta,
             nombreCliente: dataApi.cliente?.nombre_completo,
             tipoCuenta: dataApi.producto?.tipo_producto,
             tasaInteres: dataApi.producto?.tasa_interes,
             saldo: dataApi.saldo,
             estatus: dataApi.estatus
-          };
-
-          this.saldos = [cuentaMapeada];
+          }];
         } else {
           this.saldos = [];
           this.errorMsg = 'No se encontró la cuenta';
         }
-
         this.calcularTotal();
         this.loading = false;
         this.cdr.detectChanges();
       },
-      error: (err) => {
+      error: () => {
         this.errorMsg = 'Error al buscar la cuenta';
         this.loading = false;
         this.saldos = [];
         this.cdr.detectChanges();
       }
     });
+    return;
+  }
+  if (this.nombreCliente) {
+    const termino = this.nombreCliente.toLowerCase().trim();
+    this.saldos = this.saldosOriginal.filter(s =>
+      s.nombreCliente?.toLowerCase().includes(termino)
+    );
+    if (this.saldos.length === 0) this.errorMsg = 'No se encontraron cuentas para ese nombre';
+    this.calcularTotal();
+    this.cdr.detectChanges();
+    return;
   }
 
-    calcularTotal(): void {
-      this.totalSaldo = this.saldos.reduce(
-        (acc, curr) => acc + (Number(curr.saldo) || 0),
-        0
-      );
-    }
-      limpiarBusqueda(): void {
-      this.numeroCuenta = '';
-      this.errorMsg = '';
-      this.cargarSaldos();
-    }
+  this.cargarSaldos();
+}
+  calcularTotal(): void {
+    this.totalSaldo = this.saldos.reduce(
+      (acc, curr) => acc + (Number(curr.saldo) || 0), 0
+    );
+  }
+
+  limpiarBusqueda(): void {
+    this.numeroCuenta = '';
+    this.nombreCliente = '';
+    this.errorMsg = '';
+    this.saldos = this.saldosOriginal;
+    this.calcularTotal();
+    this.cdr.detectChanges();
+  }
 }
